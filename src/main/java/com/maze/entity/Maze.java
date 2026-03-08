b6257a58972063b580929d454794d9b6b3be43d5
Konrad <konradgudowski@gmail.com>
2026-03-08 20:21:01 +0100

    setup getters

---
+++ src/main/java/com/maze/entity/Maze.java
@@ -0,0 +1,15 @@
+package com.maze.entity;
+
+public class Maze {
+    private int mazeHeight, mazeWidth;
+
+    public Maze(int mazeHeight, int mazeWidth){
+        this.mazeHeight = mazeHeight;
+        this.mazeWidth = mazeWidth;
+    }
+
+    public int getMazeHeight() { return mazeHeight; }
+
+    public int getMazeWidth() {return mazeWidth;}
+
+}
