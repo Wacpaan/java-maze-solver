018c45863c194f5fb3bd22ecc8314d2ef4b7795e
Konrad <konradgudowski@gmail.com>
2026-03-08 20:34:27 +0100

    chagne to Grid layout

--- src/main/java/com/maze/entity/Maze.java
+++ src/main/java/com/maze/entity/Maze.java
@@ -3,11 +3,18 @@ package com.maze.entity;
 public class Maze {
     private int mazeHeight, mazeWidth;
     char[][] grid;
-    
+
     public Maze(int mazeHeight, int mazeWidth){
         this.mazeHeight = mazeHeight;
         this.mazeWidth = mazeWidth;
         this.grid = new char[mazeHeight][mazeWidth];
+
+    for(int x = 0; x < mazeHeight; x++){
+        for(int y = 0; y < mazeWidth; y++){
+        grid[x][y] = '#';
+        }
+    }
+
     }
 
     //Gettery
@@ -16,4 +23,5 @@ public class Maze {
 
     public int getMazeWidth() {return mazeWidth; }
 
+    public char[][] getGrid() { return grid; }
 }
--- src/main/java/com/maze/renderer/MazeRenderer.java
+++ src/main/java/com/maze/renderer/MazeRenderer.java
@@ -5,7 +5,7 @@ public class MazeRenderer {
     public void render(Maze maze){
         for(int x = 0; x < maze.getMazeHeight(); x++){
             for(int y = 0; y < maze.getMazeWidth(); y++){
-                System.out.print("#");
+                System.out.print(maze.getGrid()[x][y]);
             }
                 System.out.println("");
     }
