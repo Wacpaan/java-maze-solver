020d787df782ef5ec918c61b8904d7af256e1845
Konrad <konradgudowski@gmail.com>
2026-03-11 21:33:53 +0100

    feat add two methods, starting generating maze

--- src/main/java/com/maze/generator/MazeGenerator.java
+++ src/main/java/com/maze/generator/MazeGenerator.java
@@ -1,6 +1,37 @@
 package com.maze.generator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.maze.entity.Cell;
 import com.maze.entity.Maze;
 public class MazeGenerator {
-        public void generate(Maze maze){
+
+    public void generator(Maze maze){
+        carve(maze, 0, 0);
+
+    }
+
+    private void carve(Maze maze, int row, int col){
+        Cell cell = maze.getGrid()[row][col];
+        
+        cell.setWall(false);
+        cell.setVisited(true);
+    
+        int[] dr = {-2, 2, 0, 0}; 
+        int[] dc = {0, 0, -2, 2};
+        List<Integer> kierunki = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+
+        Collections.shuffle(kierunki);
+
+        for(int i : kierunki){
+            int newRow = row + dr[i];
+            int newCol = col + dc[i];
+
+            if(newRow >= 0 && newRow < maze.getMazeHeight() && newCol >= 0 && newCol < maze.getMazeWidth() && !maze.getGrid()[newRow][newCol].isVisited()){
+
+            }
+        }
     }
 }
