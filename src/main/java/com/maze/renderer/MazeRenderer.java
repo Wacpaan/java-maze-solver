8126e729bfdc43f22b132099e005e3cd0f47b27f
Konrad <konradgudowski@gmail.com>
2026-03-11 20:55:53 +0100

    feat: change render to base one (one cell one #)

--- src/main/java/com/maze/renderer/MazeRenderer.java
+++ src/main/java/com/maze/renderer/MazeRenderer.java
@@ -11,29 +11,17 @@ public class MazeRenderer {
     */
    
     public void render(Maze maze){
-        for(int x = 0; x < (maze.getMazeHeight()*2+1); x++){
-
-                for(int y = 0; y < (maze.getMazeWidth()*2+1); y++){
-
-                    if(x % 2 == 0 && y % 2 == 0){System.out.print("#");}
-                    else {
-                        int row = x / 2;
-                        int col = y / 2;
-                        if( x % 2 == 0 && y % 2 != 0){
-                            Cell cell = maze.getGrid()[row][col];
-                            {if (cell.getWallTop() == true){System.out.print("#");} else { System.out.print(" "); }}                        
-                    }  else if ( x % 2 != 0 && y % 2 == 0 ) {
-                        Cell cell = maze.getGrid()[row][col];
-                        if (cell.getWallLeft()){ System.out.print("#");} else {System.out.print(" ");}
-                    }   else {
-                        System.out.print(" ");
-                    }
-                }
-                }
+        for(int x = 0; x < maze.getMazeHeight(); x++){
+            for(int y = 0; y < maze.getMazeWidth(); y++){
+                Cell cell = maze.getGrid()[x][y];
+                if(cell.getWall()){ System.out.print("#"); }
+                else { System.out.print(" "); }
+            }
             System.out.println();
         }
 }
 }
+
 
 /*
 00  01  02
