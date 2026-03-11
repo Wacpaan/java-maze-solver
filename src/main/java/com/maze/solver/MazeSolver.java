bdfbaae9bbb075fc02d23225c7cc60189fe05864
Konrad <konradgudowski@gmail.com>
2026-03-11 22:11:29 +0100

    setup file

---
+++ src/main/java/com/maze/solver/MazeSolver.java
@@ -0,0 +1,17 @@
+package com.maze.solver;
+
+import com.maze.entity.*;
+
+public class MazeSolver {
+
+    public void solver(Maze maze){
+
+    }
+
+    private boolean search(Maze maze, int row, int col){
+        
+
+        return true;
+    }
+
+}
