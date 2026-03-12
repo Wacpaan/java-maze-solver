cc3d3a2ee188745a9b5386b410f02b8a939813b5
Konrad <konradgudowski@gmail.com>
2026-03-12 23:49:32 +0100

    repush to repo

--- src/main/java/com/maze/solver/MazeSolver.java
+++ src/main/java/com/maze/solver/MazeSolver.java
@@ -1,15 +1,30 @@
 package com.maze.solver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import com.maze.entity.*;
 
 public class MazeSolver {
 
     public void solver(Maze maze){
-
+        search(maze, 1, 1);
     }
 
     private boolean search(Maze maze, int row, int col){
-        
+        Cell cell = maze.getGrid()[row][col];
+
+        cell.setVisited(true);
+
+        int[] dr = {-1 , 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+        List<Integer> kierunki = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+
+        if(cell.isVisited()){
+            System.out.print("*");
+        }
 
         return true;
     }
