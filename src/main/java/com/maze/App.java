f0d3fc2a5947bab82ca40c7dd0f9729fa9c0c362
Konrad Gudowski <konradgudowski@gmail.com>
2026-03-13 14:13:46 +0000

    feat: add render maze before solver

--- src/main/java/com/maze/App.java
+++ src/main/java/com/maze/App.java
@@ -13,6 +13,7 @@ public class App
         MazeGenerator generator = new MazeGenerator();
         MazeSolver solver = new MazeSolver();
         generator.generator(maze);
+        renderer.render(maze);
         maze.resetVisited();
         solver.solver(maze);
         renderer.render(maze);
