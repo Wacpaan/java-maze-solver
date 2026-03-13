e803d0d97e00d60c44935eac8ffe808da6e0beae
Konrad Gudowski <konradgudowski@gmail.com>
2026-03-13 13:11:23 +0000

    feat: add solver method and search

--- src/main/java/com/maze/solver/MazeSolver.java
+++ src/main/java/com/maze/solver/MazeSolver.java
@@ -16,17 +16,31 @@ public class MazeSolver {
     private boolean search(Maze maze, int row, int col){
         Cell cell = maze.getGrid()[row][col];
 
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+      if(row == maze.getMazeHeight()-2 && col == maze.getMazeWidth()-2){
+        System.out.println("-----------------");
+        System.out.println("    ZNALEZIONO   ");
+        System.out.println("-----------------");
+        return true;
+      }
+
         cell.setVisited(true);
 
-        int[] dr = {-1 , 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        List<Integer> kierunki = new ArrayList<>(Arrays.asList(0, 1, 2, 3));
+      for(int i = 0; i < 4; i++){
+        int newRow = row + dr[i];
+        int newCol = col + dc[i];
 
-        if(cell.isVisited()){
-            System.out.print("*");
+        if(newRow >= 0 && newRow < maze.getMazeHeight() && newCol >= 0 && newCol < maze.getMazeWidth() && !maze.getGrid()[newRow][newCol].isVisited() && !maze.getGrid()[newRow][newCol].getWall())
+        if(search(maze,newRow,newCol)){
+          return true;
         }
+      }
 
-        return true;
+      return false;
     }
 
 }
+
+
