42b160354a3b870591383174dd15b9b89ab695f8
Konrad Gudowski <konradgudowski@gmail.com>
2026-03-13 14:13:02 +0000

    style: change language in comment

--- src/main/java/com/maze/solver/MazeSolver.java
+++ src/main/java/com/maze/solver/MazeSolver.java
@@ -9,20 +9,25 @@ import com.maze.entity.*;
 
 public class MazeSolver {
 
+
+
+  /*    DFS METHOD        */
+
     public void solver(Maze maze){
         search(maze, 1, 1);
     }
 
     private boolean search(Maze maze, int row, int col){
         Cell cell = maze.getGrid()[row][col];
-
+        
         int[] dr = {-1, 1, 0, 0};
         int[] dc = {0, 0, -1, 1};
 
       if(row == maze.getMazeHeight()-2 && col == maze.getMazeWidth()-2){
         System.out.println("-----------------");
-        System.out.println("    ZNALEZIONO   ");
+        System.out.println("      FOUND      ");
         System.out.println("-----------------");
+        cell.setPath(true);
         return true;
       }
 
@@ -34,6 +39,7 @@ public class MazeSolver {
 
         if(newRow >= 0 && newRow < maze.getMazeHeight() && newCol >= 0 && newCol < maze.getMazeWidth() && !maze.getGrid()[newRow][newCol].isVisited() && !maze.getGrid()[newRow][newCol].getWall())
         if(search(maze,newRow,newCol)){
+          cell.setPath(true);
           return true;
         }
       }
